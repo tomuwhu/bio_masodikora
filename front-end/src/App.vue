05434729f3a34fb58982a54e9e979a47124679d0
Tamás Németh <tomuwhu@gmail.com>
2019-02-14 16:40:23 +0100

    .

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -4,13 +4,22 @@
     <br>
     <input v-model="i1" @change="add()">
     <button @click="add()">Katt ide</button>
-    <div :id="i" :key="i" v-for="(elem,i) in t">
+    <div :id="i" :key="i" v-for="(elem,i) in t" class="cuccos">
       <hr>
       <table>
-        <td :key="j" v-for="(betu,j) in elem">{{betu}} - {{betu.toUpperCase()}}</td>
+        <td class="betus" :key="j" v-for="(betu,j) in elem">{{betu}} - {{betu.toUpperCase()}}</td>
       </table>
     </div>  
-    
+    <hr>
+    <h2>Amőba</h2>
+    <table>
+      <tr :key="i" v-for="(sor,i) in amoba">
+        <td :class="'a '+cella" 
+            :key="j" 
+            v-for="(cella,j) in sor"
+            @click="f(i,j)">{{cella}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -20,8 +29,13 @@ export default {
   data: () => ({
     i1: '',
     t: [],
+    v: 'X',
+    amoba: Array(20).fill(0).map( () => Array(20).fill(0).map( () => ' '))
   }),
   methods: {
+    f(x,y) {
+      this.$set(this.amoba[x],y,this.v=this.v==="X"?"O":"X")
+    },
     add() {
       this.t.push(this.i1)
       this.t = this.t.sort( (a,b) => a.localeCompare(b) )
@@ -32,11 +46,39 @@ export default {
 </script>
 
 <style>
-td {
+@import url('https://fonts.googleapis.com/css?family=Felipa');
+td.betus {
   border: solid 1px #47856d;
   color: #133846;
   width: 50px;
   height: 20px;
   text-align: center;
+}
+td.a {
+  border: solid 1px #47856d;
+  color: #133846;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  border-radius: 6px;
+  cursor: pointer;
+}
+td.X {
+  background-color: #77aaf7af; 
+}
+td.O {
+  background-color: #f77659; 
+}
+h2 {
+  font-family: 'Felipa', cursive;
+  color:#207088;
+  text-shadow: 1px 1px 3px;
+}
+div.cuccos {
+  box-shadow: 1px 1px 4px;
+  border-radius: 12px; 
+  padding: 10px;
+  margin:7px ;
+  background-color: #ddd7c7; 
 }
 </style>
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('front-end/dist'))
 
 app.get('/cica', (req, res) => res.send('Hello Macska!'))
 
